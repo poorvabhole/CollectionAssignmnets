c667b32e5d2315f8f4afde5d94a9f2a24c13ebe0
poorvabhole <poorva.bhole@cognologix.com>
2022-09-16 18:52:53 +0530

    new update

--- src/main/java/com/collectionpart2/questionnine/checkIfAnagramOrNot.java
+++ src/main/java/com/collectionpart2/questionnine/checkIfAnagramOrNot.java
@@ -5,42 +5,62 @@ import java.util.*;
 
 public class checkIfAnagramOrNot {
     public static void main(String[] args) {
-        String input = "dormitory";
-//        String input = "listen";
+        checkIfAnagramOrNot anagramOrNot = new checkIfAnagramOrNot();
+//        String input = "dormitory";
+        String input = "listen";
         String[] candidates = {"enlists", "google", "inlets", "dirty room"};
 
-        List<String> list = anagramList(input, candidates);
+        List<String> list = anagramOrNot.anagramList(input, candidates);
         System.out.println(list);
 
     }
-
-    private static List<String> anagramList(String inputWord, String[] candidates)
+    private List<String> anagramList(String inputWord, String[] candidates)
     {
         List<String> list = new ArrayList<String>();
 
-        char[] inputChars = inputWord.toCharArray();
-//        List<Character> inputCharList = Arrays.asList(inputChars);
-//        char[] charArray = str.toCharArray();
-//        Character[] charObjectArray = ArrayUtils.toObject(inputChars);
-        Set<Character> keySet = new HashSet<>();
+        Character[] inputWordLetters = inputWord.chars()
+                                        .mapToObj(c -> (char) c)
+                                        .toArray(Character[]::new);
 
-//        for (Character ch : inputWord.toCharArray())
-//        {
-//            keySet.add(ch);
-//        }
-        for (String word : candidates)
-        {
-            Set<Character> keySet2 = new HashSet<>();
-            for (Character ch : word.replaceAll("\\s", "").toCharArray())
-            {
-                keySet2.add(ch);
-            }
-            if (keySet.size() == keySet2.size())
-            {
+        Set<Character> letterSet = new HashSet<>(Arrays.asList(inputWordLetters));
+        System.out.println(letterSet );
+
+        for (String word : candidates ) {
+            Character[] charactersInWord = word.chars()
+                                            .mapToObj(c -> (char) c)
+                                            .toArray(Character[]::new);
+            Set<Character> wordLetterSet = new HashSet<>(Arrays.asList(charactersInWord));
+            if (letterSet.size() == wordLetterSet.size()){
                 list.add(word);
             }
         }
         return list;
+
+//        char[] inputChars = inputWord.toCharArray();
+//        List<Character> inputCharList = Arrays.asList(inputChars);
+//        char[] charArray = str.toCharArray();
+//        Character[] charObjectArray = ArrayUtils.toObject(inputChars);
+
+//        String word = "word";
+        
+//        Set<Character> keySet = new HashSet<>();
+//        for (Character ch : inputWord.toCharArray())
+//        {
+//            keySet.add(ch);
+//        }
+//        for (String word : candidates)
+//        {
+//            Set<Character> keySet2 = new HashSet<>();
+//            for (Character ch : word.replaceAll("\\s", "").toCharArray())
+//            {
+//                keySet2.add(ch);
+//            }
+//            if (keySet.size() == keySet2.size())
+//            {
+//                list.add(word);
+//            }
+//        }
+
 
 //        char[] charsInWord = inputWord.toCharArray();
 //        Arrays.sort(charsInWord);
